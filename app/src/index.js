30b5b6f912ca50f854b8edb08be8ac03415e843d
imartinezl <inigomlap@gmail.com>
2019-11-29 09:24:46 +0100

    Initial CSS

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import AppController from './AppController';
+import './style.css';
 
 document.addEventListener("DOMContentLoaded", () => {
     let appController = new AppController();
