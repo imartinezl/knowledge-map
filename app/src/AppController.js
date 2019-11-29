e01bdea096eab671d37b40823e3af45c7b92f285
imartinezl <inigomlap@gmail.com>
2019-11-29 09:25:37 +0100

    Minimal working example

--- app/src/AppController.js
+++ app/src/AppController.js
@@ -1,7 +1,8 @@
 "use strict";
 const d3 = require('d3');
-const flextree = require('d3-flextree').flextree;
-const markmap = require('markmap/lib/d3-flextree');
+// const flextree = require('d3-flextree').flextree;
+require('markmap/lib/d3-flextree')
+const markmap = require('markmap/lib/view.mindmap');
 const parse = require('markmap/lib/parse.txtmap');
 const transform = require('markmap/lib/transform.headings');
 
@@ -14,17 +15,16 @@ export default class FController {
 
     init() {
 
-        console.log("HEYUs");
-
-        d3.text("./assets/example.txtmap", function (error, text) {
-            console.log(error, text);
-            if (error) throw error;
-            console.log("Hey2")
+        console.log("HEY");
+        d3.text("assets/example.txtmap").then((text) => {
+            // console.log(text); // Hello, world!
             const data = transform(parse(text));
+            // console.log(data);
             markmap('svg#mindmap', data, {
                 preset: 'default', // or default
                 linkShape: 'bracket' // or bracket
             });
         });
+
     }
 }
