75b3e1097a4b63b2848f8e99b3d4f3596402f188
imartinezl <inigomlap@gmail.com>
2019-11-29 14:03:26 +0100

    Import library from lib, using ES6

--- app/src/AppController.js
+++ app/src/AppController.js
@@ -1,10 +1,17 @@
 "use strict";
-const d3 = require('d3');
+
+import * as d3 from 'd3';
 // const flextree = require('d3-flextree').flextree;
-require('markmap/lib/d3-flextree')
-const markmap = require('markmap/lib/view.mindmap');
-const parse = require('markmap/lib/parse.txtmap');
-const transform = require('markmap/lib/transform.headings');
+import Markmap from './lib/view.mindmap';
+
+//require('markmap/lib/d3-flextree');
+// import './lib/d3-flextree.js';
+
+// const markmap = require('markmap/lib/view.mindmap');
+// import markmap from './lib/view.mindmap';
+
+import parse from './lib/parse.txtmap';
+import transform from './lib/transform.headings';
 
 
 export default class FController {
@@ -16,15 +23,25 @@ export default class FController {
     init() {
 
         console.log("HEY");
-        d3.text("assets/example.txtmap").then((text) => {
-            // console.log(text); // Hello, world!
+        d3.text("assets/map.txt", (error, text) => {
             const data = transform(parse(text));
-            // console.log(data);
-            markmap('svg#mindmap', data, {
-                preset: 'default', // or default
-                linkShape: 'bracket' // or bracket
-            });
+            console.log(data);
+            Markmap('svg#mindmap', data, {
+                preset: 'default', // or default / colorful
+                linkShape: 'diagonal', // or bracket / diagonal
+            });        
         });
+        // d3.text("assets/map.txtmap").then((text) => {
+        //     // console.log(text); // Hello, world!
+        //     const data = transform(parse(text));
+        //     console.log(data);
+        //     console.log(Markmap);
+        //     Markmap('svg#mindmap', data, {
+        //         preset: 'default', // or default / colorful
+        //         linkShape: 'diagonal', // or bracket / diagonal
+
+        //     });
+        // });
 
     }
 }
