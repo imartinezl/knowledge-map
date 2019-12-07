7a56ecb6076c4eb192f51315e503e3919ce97cab
imartinezl <inigomlap@gmail.com>
2019-12-07 23:16:18 +0100

    Color pickr options

--- app/src/lib/ColorPicker.js
+++ app/src/lib/ColorPicker.js
@@ -4,50 +4,63 @@ import Pickr from '@simonwep/pickr';
 
 export default class ColorPicker {
 
-    constructor() {
-        const pickr = Pickr.create({
-            el: '.color-picker',
+    constructor(el, defColor) {
+        this.init(el, defColor);
+        this.initEvents();
+    }
+    
+    init(el, defColor) {
+        this.pickr = Pickr.create({
+            el: el,
             theme: 'nano', // 'classic' or 'monolith', or 'nano'
             swatches: [
                 'rgba(244, 67, 54, 1)',
-                'rgba(233, 30, 99, 0.95)',
-                'rgba(156, 39, 176, 0.9)',
-                'rgba(103, 58, 183, 0.85)',
-                'rgba(63, 81, 181, 0.8)',
-                'rgba(33, 150, 243, 0.75)',
-                'rgba(3, 169, 244, 0.7)',
-                'rgba(0, 188, 212, 0.7)',
-                'rgba(0, 150, 136, 0.75)',
-                'rgba(76, 175, 80, 0.8)',
-                'rgba(139, 195, 74, 0.85)',
-                'rgba(205, 220, 57, 0.9)',
-                'rgba(255, 235, 59, 0.95)',
+                'rgba(233, 30, 99, 1)',
+                'rgba(156, 39, 176, 1)',
+                'rgba(103, 58, 183, 1)',
+                'rgba(63, 81, 181, 1)',
+                'rgba(33, 150, 243, 1)',
+                'rgba(3, 169, 244, 1)',
+                'rgba(0, 188, 212, 1)',
+                'rgba(0, 150, 136, 1)',
+                'rgba(76, 175, 80, 1)',
+                'rgba(139, 195, 74, 1)',
+                'rgba(205, 220, 57, 1)',
+                'rgba(255, 235, 59, 1)',
                 'rgba(255, 193, 7, 1)'
             ],
-
+            container: 'body',
+            default: defColor,
+            lockOpacity: true,
+            useAsButton: false,
             components: {
-
                 // Main components
                 preview: true,
                 opacity: true,
                 hue: true,
-
                 // Input / output Options
                 interaction: {
                     hex: true,
                     rgba: true,
-                    hsla: true,
-                    hsva: true,
-                    cmyk: true,
+                    hsla: false,
+                    hsva: false,
+                    cmyk: false,
                     input: true,
-                    clear: true,
-                    save: true
+                    clear: false,
+                    save: false
                 }
             }
         });
-
+        this.pickr.setColor(defColor);
     }
 
-    init() {
+    initEvents(){
+        // this.pickr.on('change', (color, instance) => {
+        //     console.log('change', color, instance);
+        // })
+    }
+
+    getHEXAColor(){
+        return this.pickr.getColor().toHEXA().toString();
     }
 }
