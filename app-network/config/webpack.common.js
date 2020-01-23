265c8b970e7fbae5620dbbcfce8566ae9648d0e2
imartinezl <inigomlap@gmail.com>
2020-01-23 08:28:59 +0100

    app-network: minor changes

--- app-network/config/webpack.common.js
+++ app-network/config/webpack.common.js
@@ -4,124 +4,122 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  /**
-   * Entry
-   *
-   * The first place Webpack looks to start building the bundle.
-   */
-  entry: [paths.src + '/index.js'],
-
-  /**
-   * Output
-   *
-   * Where Webpack outputs the assets and bundles.
-   */
-  output: {
-    path: paths.build,
-    filename: '[name].bundle.js',
-    publicPath: '/',
-  },
-
-  /**
-   * Plugins
-   *
-   * Customize the Webpack build process.
-   */
-  plugins: [
     /**
-     * CleanWebpackPlugin
+     * Entry
      *
-     * Removes/cleans build folders and unused assets when rebuilding.
+     * The first place Webpack looks to start building the bundle.
      */
-    new CleanWebpackPlugin(),
+    entry: [paths.src + '/index.js'],
 
     /**
-     * CopyWebpackPlugin
+     * Output
      *
-     * Copies files from target to destination folder.
+     * Where Webpack outputs the assets and bundles.
      */
-    new CopyWebpackPlugin([
-      {
-        from: paths.static,
-        to: 'assets',
-        ignore: ['*.DS_Store'],
-      },
-    ]),
+    output: {
+        path: paths.build,
+        filename: '[name].bundle.js',
+        publicPath: '/',
+    },
 
     /**
-     * HtmlWebpackPlugin
+     * Plugins
      *
-     * Generates an HTML file from a template.
+     * Customize the Webpack build process.
      */
-    new HtmlWebpackPlugin({
-      title: 'Webpack Boilerplate',
-      favicon: paths.src + '/images/favicon.png',
-      template: paths.src + '/template.html', // template file
-      filename: 'index.html', // output file
-    }),
-  ],
+    plugins: [
+        /**
+         * CleanWebpackPlugin
+         *
+         * Removes/cleans build folders and unused assets when rebuilding.
+         */
+        new CleanWebpackPlugin(),
 
-  /**
-   * Module
-   *
-   * Determine how modules within the project are treated.
-   */
-  module: {
-    rules: [
-      /**
-       * JavaScript
-       *
-       * Use Babel to transpile JavaScript files.
-       */
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader'],
-      },
+        /**
+         * CopyWebpackPlugin
+         *
+         * Copies files from target to destination folder.
+         */
+        new CopyWebpackPlugin([{
+            from: paths.static,
+            to: 'assets',
+            ignore: ['*.DS_Store'],
+        }, ]),
 
-      /**
-       * Styles
-       *
-       * Inject CSS into the head with source maps.
-       */
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          'style-loader',
-          { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
-          { loader: 'postcss-loader', options: { sourceMap: true } },
-          { loader: 'sass-loader', options: { sourceMap: true } },
-        ],
-      },
-
-      /**
-       * Images
-       *
-       * Copy image files to build folder.
-       */
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg|webp|svg)$/i,
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[ext]',
-          context: 'src', // prevent display of src/ in filename
-        },
-      },
-
-      /**
-       * Fonts
-       *
-       * Inline font files.
-       */
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 8192,
-          name: '[path][name].[ext]',
-          context: 'src', // prevent display of src/ in filename
-        },
-      },
+        /**
+         * HtmlWebpackPlugin
+         *
+         * Generates an HTML file from a template.
+         */
+        new HtmlWebpackPlugin({
+            title: 'DS Knowledge Graph',
+            favicon: paths.src + '/images/favicon.png',
+            template: paths.src + '/template.html', // template file
+            filename: 'index.html', // output file
+        }),
     ],
-  },
+
+    /**
+     * Module
+     *
+     * Determine how modules within the project are treated.
+     */
+    module: {
+        rules: [
+            /**
+             * JavaScript
+             *
+             * Use Babel to transpile JavaScript files.
+             */
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'eslint-loader'],
+            },
+
+            /**
+             * Styles
+             *
+             * Inject CSS into the head with source maps.
+             */
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    'style-loader',
+                    { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
+                    { loader: 'postcss-loader', options: { sourceMap: true } },
+                    { loader: 'sass-loader', options: { sourceMap: true } },
+                ],
+            },
+
+            /**
+             * Images
+             *
+             * Copy image files to build folder.
+             */
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg|webp|svg)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]',
+                    context: 'src', // prevent display of src/ in filename
+                },
+            },
+
+            /**
+             * Fonts
+             *
+             * Inline font files.
+             */
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 8192,
+                    name: '[path][name].[ext]',
+                    context: 'src', // prevent display of src/ in filename
+                },
+            },
+        ],
+    },
 }
